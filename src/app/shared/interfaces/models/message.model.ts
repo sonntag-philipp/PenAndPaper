d891837891e29ec74a44deae4d576430bb352792
BrainDelaiy / Philipp Sonntag <sonntag.philipp@outlook.de>
2017-08-30 19:02:01 +0200

    Added fxFlex into app-component for clearness

---
+++ src/app/shared/interfaces/models/message.model.ts
@@ -0,0 +1,5 @@
+export interface IMessageModel {
+
+  content: string;
+
+}
--- src/app/shared/services/backend.service.ts
+++ src/app/shared/services/backend.service.ts
@@ -1,3 +1,19 @@
+import { IMessageModel } from '../interfaces/models/message.model';
+import { Observable } from 'rxjs/Rx';
+import { Headers, Http, RequestOptionsArgs, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+@Injectable()
 export class BackendService {
+
+  private requestOptions: RequestOptionsArgs;
+
+  constructor(private http: Http) { }
   
+
+  public sendRequest(msg: IMessageModel): Observable<Response> {
+
+    this.requestOptions.headers = new Headers({'Content-Type': 'application/json'});
+
+    return this.http.post("http://localhost:4200", msg, this.requestOptions);
+  }
 }
